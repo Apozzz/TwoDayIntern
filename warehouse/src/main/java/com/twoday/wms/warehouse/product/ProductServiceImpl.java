e1f03c7e49a553a96af3bab27b0bb889f138edf0
Arnas Bosas <arnas.bosas@twoday.com>
2023-08-15 22:32:07 +0300

    Fixed wrong product saving method

--- warehouse/src/main/java/com/twoday/wms/warehouse/product/ProductServiceImpl.java
+++ warehouse/src/main/java/com/twoday/wms/warehouse/product/ProductServiceImpl.java
@@ -43,7 +43,7 @@ public class ProductServiceImpl implements ProductService {
 
         product.setQuantity(product.getQuantity() - quantity);
         User user = userRepository.findByUsername(username)
-            .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
         Purchase purchase = new Purchase(user, product, quantity, LocalDateTime.now());
         purchaseRepository.save(purchase);
@@ -63,15 +63,18 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductDto saveProductByWarehouseId(Long id, ProductDto productDto) {
-        Product product = productConverter.fromDto(productDto);
-        return warehouseRepository.findById(id).map(
-                warehouse -> {
-                    warehouse.addProduct(product);
-                    warehouseRepository.save(warehouse);
+        if (productDto == null) {
+            throw new IllegalArgumentException("ProductDto cannot be null");
+        }
 
-                    return productDto;
-                })
+        Warehouse warehouse = warehouseRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Warehouse with id: %s was not found".formatted(id)));
+
+        Product product = productConverter.fromDto(productDto);
+        warehouse.addProduct(product);
+        warehouseRepository.save(warehouse);
+
+        return productDto;
     }
 
 }
