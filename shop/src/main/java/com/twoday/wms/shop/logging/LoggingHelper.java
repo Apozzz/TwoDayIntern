06fa6638ddd6e88da3cef2579730faf5e77799e7
Arnas Bosas <arnas.bosas@twoday.com>
2023-08-16 13:21:35 +0300

    Refactored AOP so it doesn't use boilerplates and unneccessary code

--- shop/src/main/java/com/twoday/wms/shop/logging/LoggingHelper.java
+++ shop/src/main/java/com/twoday/wms/shop/logging/LoggingHelper.java
@@ -3,6 +3,7 @@ package com.twoday.wms.shop.logging;
 import java.lang.reflect.Method;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 
@@ -18,35 +19,40 @@ public class LoggingHelper {
     }
 
     public static LogMessage getLogMessageAnnotation(JoinPoint joinPoint) {
-        Method method = getMethodFromJoinPoint(joinPoint);
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
         return method.getAnnotation(LogMessage.class);
     }
 
     public static void logBefore(JoinPoint joinPoint, Logger logger) {
-        LogMessage logMessageAnnotation = LoggingHelper.getLogMessageAnnotation(joinPoint);
-
-        if (logMessageAnnotation != null && !logMessageAnnotation.before().isEmpty()) {
-            logger.info(logMessageAnnotation.before(), joinPoint.getArgs());
-        } else {
-            LoggerUtil.logInfo(logger, DEFAULT_BEFORE_MESSAGE, joinPoint.getSignature().toShortString(),
-                    joinPoint.getArgs());
-        }
+        LogMessage logMessage = getLogMessageAnnotation(joinPoint);
+        String message = logMessage.before().isEmpty() ? DEFAULT_BEFORE_MESSAGE : logMessage.before();
+        logWithSafetyCheck(joinPoint, logger, message, joinPoint.getArgs());
     }
 
     public static void logAfter(JoinPoint joinPoint, Logger logger) {
-        LogMessage logMessageAnnotation = LoggingHelper.getLogMessageAnnotation(joinPoint);
-
-        if (logMessageAnnotation != null && !logMessageAnnotation.after().isEmpty()) {
-            logger.info(logMessageAnnotation.after(), joinPoint.getArgs());
-        } else {
-            LoggerUtil.logInfo(logger, DEFAULT_AFTER_MESSAGE, joinPoint.getSignature().toShortString(),
-                    joinPoint.getArgs());
-        }
+        LogMessage logMessage = getLogMessageAnnotation(joinPoint);
+        String message = logMessage.after().isEmpty() ? DEFAULT_AFTER_MESSAGE : logMessage.after();
+        logWithSafetyCheck(joinPoint, logger, message, joinPoint.getArgs());
     }
 
-    private static Method getMethodFromJoinPoint(JoinPoint joinPoint) {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        return methodSignature.getMethod();
+    public static void logException(JoinPoint joinPoint, Logger logger, Throwable ex) {
+        logWithSafetyCheck(joinPoint, logger, "An error occurred in method {}. Details: {}", ex.getMessage());
+    }
+
+    private static String safeToShortString(JoinPoint joinPoint) {
+        Signature signature = joinPoint != null ? joinPoint.getSignature() : null;
+        return signature != null ? signature.toShortString() : null;
+    }
+
+    private static void logWithSafetyCheck(JoinPoint joinPoint, Logger logger, String message, Object... args) {
+        String methodSignature = safeToShortString(joinPoint);
+        if (methodSignature != null) {
+            Object[] extendedArgs = new Object[args.length + 1];
+            extendedArgs[0] = methodSignature;
+            System.arraycopy(args, 0, extendedArgs, 1, args.length);
+            logger.info(message, extendedArgs);
+        }
     }
 
 }
