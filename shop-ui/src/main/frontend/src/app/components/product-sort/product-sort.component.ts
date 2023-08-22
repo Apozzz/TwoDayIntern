d1f08636e63146a7bd382d68b744d9c7fbfc4e7e
Arnas Bosas <arnas.bosas@twoday.com>
2023-08-22 14:34:00 +0300

    Fixed module name, added fonts and styles through npm, minor changes

---
+++ shop-ui/src/main/frontend/src/app/components/product-sort/product-sort.component.ts
@@ -0,0 +1,22 @@
+import { Component, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-product-sort',
+  templateUrl: './product-sort.component.html',
+  styleUrls: ['./product-sort.component.less']
+})
+export class ProductSortComponent {
+
+  private sortChangedSubject = new Subject<string>();
+  @Output() sortChanged = this.sortChangedSubject.pipe(debounceTime(1000));
+  sortOption: string = 'name-asc';
+
+  onSortChange() {
+    this.sortChangedSubject.next(
+      this.sortOption,
+    );
+  }
+
+}
